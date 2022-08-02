444f5b4709802a5339183a95ca45fe1b897eb04a
VERNON HEZRON <103187250+HeTheGYPSY@users.noreply.github.com>
2022-08-02 22:21:49 +0300

    C#

--- VernonNet13/VernonNet13.cpp
+++ VernonNet13/VernonNet13.cpp
@@ -1,29 +1,9 @@
-#include <iostream>
+#include <iostream> 
 
-using namespace std;
+using namespace std; 
 
-double calling()
+int main ()
 {
-    int number;
-    cout << "Enter a number:";
-    cin >> number;
-    if (number < 15)
-    {
-        cout << "Good Choice!" << endl;
-    }
-    else if (number >15 and number <100)
-    {
-        cout << "Nice try";
-    }
-    else
-    {
-        cout << "It\'s over!";
-    }
-    return 0;
-}
-
-int main()
-{
-    calling();
-    return 0;
+    cout << "Hello World!";
+    return 0; 
 }
